5e312d9304314d7a7c42e61a8b182bae05f4720e
Keli Booster <kbooster17@gmail.com>
2021-10-18 21:04:01 +0000

    addomponents

--- screens/StartGame.js
+++ screens/StartGame.js
@@ -1,28 +1,32 @@
-import React from 'react'
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
 const StartGame = (props) => {
-    return (
-        <View style={styles.screen}>
-            <Text>Start a new game.</Text>
-            <Text>Select a new number</Text>
-            <TextInput />
-            <View style={styles.buttonContainer}>
-                <Button title="Reset" onPress={() => {
-
-                }} />
-                <Button title="Confirm" onPress={() => {
-
-                }} />
-            </View>
-        </View>
-    )
-}
+  return (
+    <View style={styles.screen}>
+      <Text>Start a new game.</Text>
+      <Text>Select a new number</Text>
+      <TextInput style={styles.textInp} />
+      <View style={styles.buttonContainer}>
+        <Button title="Reset" onPress={() => {}} />
+        <Button title="Confirm" onPress={() => {}} />
+      </View>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        alignItems: "center",
-        padding: 10
-    }
-})
-export default StartGame
+  screen: {
+    flex: 1,
+    alignItems: "center",
+    padding: 10,
+  },
+//   textInp: {
+//     margin: "10",
+//     shadowColor: "#000",
+//     shadowOffset: {
+//       width: 0,
+//       height: 2,
+//     },
+//   },
+});
+export default StartGame;
